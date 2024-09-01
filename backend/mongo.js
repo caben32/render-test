7a7bb0960c8981f34b8cd4e06fe79e22d85732ed
caben32 <cabenano@gmail.com>
2024-09-01 19:46:36 -0300

    Adding tests and also login

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,44 +1,39 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
-const name = process.argv[3] ? process.argv[3] : 'Did not provide'
-const number = process.argv[4] ? process.argv[4] : 'Did not provide'
 
 const url =
-  `mongodb+srv://cabenano:${password}@cluster0.yahlnec.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=Cluster0`
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  })
 
-mongoose.connect(url)
+  const Note = mongoose.model('Note', noteSchema)
 
-const phoneBookSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  /*
+  const note = new Note({
+    content: 'HTML is x',
+    important: true,
+  })
+
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  */
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
 })
-
-const Person = mongoose.model('Person', phoneBookSchema)
-
-if (process.argv.length==3){
-    Person.find({}).then(result => {
-        result.forEach(note => {
-          console.log(note)
-        })
-        mongoose.connection.close()
-      })
-}
-else{
-    const person = new Person({
-        name: name,
-        number: number,
-    })
-    
-    person.save().then(result => {
-      console.log('Person saved!')
-      mongoose.connection.close()
-    })
-}
